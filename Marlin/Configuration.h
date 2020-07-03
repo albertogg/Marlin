3fe94d77c75aff70d4ab6bceef1315deacc7f928
Alberto Grespan <alberto@albertogrespan.com>
2020-07-02 23:45:21 -0400

    Update PETG config

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1485,8 +1485,8 @@
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
 
 #define PREHEAT_2_LABEL       "PETG"
-#define PREHEAT_2_TEMP_HOTEND 235
-#define PREHEAT_2_TEMP_BED     70
+#define PREHEAT_2_TEMP_HOTEND 220
+#define PREHEAT_2_TEMP_BED     80
 #define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
 
 /**
