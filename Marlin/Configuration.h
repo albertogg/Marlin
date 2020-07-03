0bba0caae1e2396a98fb2fec599f5a3052892beb
Alberto Grespan <alberto@albertogrespan.com>
2020-07-02 23:45:07 -0400

    PID tunned for printing PETG

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -483,9 +483,10 @@
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
   // Creality Ender-3
-  #define DEFAULT_Kp 21.73
-  #define DEFAULT_Ki 1.54
-  #define DEFAULT_Kd 76.55
+  // PID tuned for 220 Celsius
+  #define DEFAULT_Kp 26.53
+  #define DEFAULT_Ki 2.50
+  #define DEFAULT_Kd 70.40
 
   // Ultimaker
   //#define DEFAULT_Kp 22.2
@@ -539,9 +540,10 @@
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
+  // PID tuned for 80 Celsius
+  #define DEFAULT_bedKp 211.99
+  #define DEFAULT_bedKi 42.36
+  #define DEFAULT_bedKd 707.35
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
